9b60ecf811b1b3f7b4a71c96cf463658ceeec4a3
Nicolau Atala <nicolau.atala@hotmail.com>
2020-01-12 19:37:20 -0300

    update: day 4 content

--- introduction-to-swift/Day-4.playground/Contents.swift
+++ introduction-to-swift/Day-4.playground/Contents.swift
@@ -40,3 +40,31 @@ while number <= 20 {
 }
 
 print("Ready or not, here I come!")
+
+/*
+ - Repeat loops
+
+ */
+
+var number2 = 1
+
+repeat {
+    print(number2)
+    number2 += 1
+} while number2 <= 20
+print("Ready or not, here I come!")
+
+//Because the condition comes at the end of the repeat loop the code inside the loop will always be executed at least once, whereas while loops check their condition before their first run.
+
+//For example, this print() function will never be run, because false is always false:
+
+while false {
+    print("This is false")
+}
+//Xcode will even warn us that the print() line will never be executed.
+
+//On the other hand, this print() function will be run once, because repeat only fails the condition after the loop runs:
+
+repeat {
+    print("This is false")
+} while false
