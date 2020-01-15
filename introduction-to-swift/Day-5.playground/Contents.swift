afcb486a37330a37f2f8408dca49624b68fff621
Nicolau Atala <nicolau.atala@hotmail.com>
2020-01-14 21:47:26 -0300

    feat: add day 5 content

---
+++ introduction-to-swift/Day-5.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// Writing functions
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+    print(message)
+}
+//We can now run that using printHelp() by itself:
+
+printHelp()
