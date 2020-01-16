aaa99cf208cafcd8c3208c5fe1524761c84fb431
Nicolau Atala <nicolau.atala@hotmail.com>
2020-01-16 20:41:51 -0300

    update: day 5 content

--- introduction-to-swift/Day-5.playground/Contents.swift
+++ introduction-to-swift/Day-5.playground/Contents.swift
@@ -21,3 +21,22 @@ func square(number: Int) {
 //That tells Swift we expect to receive an Int, and it should be called number. This name is used both inside the function when you want to refer to the parameter, but also when you run the function, like this:
 
 square(number: 8)
+
+//Returning values
+
+func square2(number: Int) -> Int {
+    return number * number
+}
+//Now we can grab that return value when the function is run, and print it there:
+
+let result = square2(number: 8)
+print(result)
+
+//Parameter labels
+func sayHello(to name: String) {
+    print("Hello, \(name)!")
+}
+
+sayHello(to: "Taylor")
+
+
