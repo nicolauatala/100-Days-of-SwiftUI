e5b9fdfcdccd6e569927145d2ba915e7b0ba8f4a
Nicolau Atala <nicolau.atala@hotmail.com>
2020-01-24 21:04:47 -0300

    update: add day 9 content

--- introduction-to-swift/Day-9.playground/Contents.swift
+++ introduction-to-swift/Day-9.playground/Contents.swift
@@ -29,4 +29,65 @@ user.username
 /*
  - Referring to the current instance
 
+ Inside methods you get a special constant called self, which points to whatever instance of the struct is currently being used. This self value is particularly useful when you create initializers that have the same parameter names as your property.
+ 
+ For example, if you create a Person struct with a name property, then tried to write an initializer that accepted a name parameter, self helps you distinguish between the property and the parameter – self.name refers to the property, whereas name refers to the parameter.
+ 
+
  */
+
+struct Person {
+    var name: String
+    
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+/*
+ - Lazy properties
+ As a performance optimization, Swift lets you create some properties only when they are needed. As an example, consider this FamilyTree struct – it doesn’t do much, but in theory creating a family tree for someone takes a long time:
+ */
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+struct Person {
+    var name: String
+    var familyTree = FamilyTree()
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person(name: "Ed")
+//But what if we didn’t always need the family tree for a particular person? If we add the lazy keyword to the familyTree property, then Swift will only create the FamilyTree struct when it’s first accessed:
+
+//lazy var familyTree = FamilyTree()
+//ed.familyTree
+
+
+/*
+ - Static properties and methods
+ You can also ask Swift to share specific properties and methods across all instances of the struct by declaring them as static.
+ 
+ To try this out, we’re going to add a static property to the Student struct to store how many students are in the class. Each time we create a new student, we’ll add one to it:
+ */
+
+struct Student {
+    static var classSize = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+print(Student.classSize)
+
