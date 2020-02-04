8c98dcc35a3174447d448d5eae4030e5c4eb2c6c
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-04 16:53:03 -0300

    update: day 17 - Adding a segmented control for tip percentages

--- starting-swiftUI/WeSplit/WeSplit/ReadingTextFromUser.swift
+++ starting-swiftUI/WeSplit/WeSplit/ReadingTextFromUser.swift
@@ -43,6 +43,14 @@ struct ReadingTextFromUser: View {
                     }
                  }
 
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
                 /*
                  
                  This synchronization happens because:
