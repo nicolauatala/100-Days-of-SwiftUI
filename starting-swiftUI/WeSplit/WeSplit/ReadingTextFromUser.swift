2b04a9d9689ddfdfced5deca62d0bc5dcbe06b3d
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-04 16:33:09 -0300

    feat: add day 17 - Reading text from the user with TextField

---
+++ starting-swiftUI/WeSplit/WeSplit/ReadingTextFromUser.swift
@@ -0,0 +1,58 @@
+//
+//  ReadingTextFromUser.swift
+//  WeSplit
+//
+//  Created by Nicolau Atala Pelluzi on 04/02/20.
+//  Copyright © 2020 Nicolau Atala Pelluzi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ReadingTextFromUser: View {
+    
+    /*
+     
+     Hopefully already you can see that means we need to add three @State properties, because there are three pieces of data we’re expecting users to enter into our app.
+
+     So, start by adding these three properties to our ContentView struct:
+     */
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    var body: some View {
+         Form {
+            
+            /*
+             One of the great things about the @State property wrapper is that it automatically watches for changes, and when something happens it will automatically re-invoke the body property. That’s a fancy way of saying it will reload your UI to reflect the changed state, and it’s a fundamental feature of the way SwiftUI works.
+
+             To demonstrate this, add a second section with a text view showing the value of checkAmount, like this:
+             */
+             Section {
+                 TextField("Amount", text: $checkAmount)
+             }
+
+            /*
+             
+             This synchronization happens because:
+
+             Our text field has a two-way binding to the checkAmount property.
+             The checkAmount property is marked with @State, which automatically watches for changes in the value.
+             When an @State property changes SwiftUI will re-invoke the body property (i.e., reload our UI)
+             Therefore the text view will get the updated value of checkAmount.
+             The final project won’t show checkAmount in that text view, but it’s good enough for now. Before we move on, though, I want to address one important problem: when you tap to enter text into our text field, users see a regular alphabetical keyboard. Sure, they can tap a button on the keyboard to get to the numbers screen, but it’s annoying and and not really necessary.
+             */
+             Section {
+                 Text("$\(checkAmount)")
+                    .keyboardType(.decimalPad)
+             }
+         }
+    }
+}
+
+struct ReadingTextFromUser_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadingTextFromUser()
+    }
+}
