425925577fa2b37c152962d38322a4b8cb7799c2
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-12 22:40:38 -0300

    update: content day 26

--- expanding-your-skills/BetterRest/BetterRest/ContentView.swift
+++ expanding-your-skills/BetterRest/BetterRest/ContentView.swift
@@ -13,6 +13,23 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+	
+	/*
+	let now = Date()
+	let tomorrow = Date().addingTimeInterval(86400)
+	let range = now ... tomorrow
+	
+	
+	var components = DateComponents()
+	components.hour = 8
+	components.minute = 0
+	let date = Calendar.current.date(from: components) ?? Date()
+
+
+	let formatter = DateFormatter()
+	formatter.timeStyle = .short
+	let dateString = formatter.string(from: Date())
+	*/
     
     var body: some View {
        NavigationView {
