334bf62d8bca61234f49b99156e6bb382966cafe
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-26 20:42:21 -0300

    feat: add day 29 content - Word Scramble project

---
+++ expanding-your-skills/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Nicolau Atala on 26/02/20.
+//  Copyright © 2020 Nicolau Atala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
