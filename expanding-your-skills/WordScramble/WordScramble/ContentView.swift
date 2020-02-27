f7e0ad7c6483896bb8a9039c59a597b83fe15179
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-26 22:30:38 -0300

    feat: add day 30 content - Adding to a list of words

--- expanding-your-skills/WordScramble/WordScramble/ContentView.swift
+++ expanding-your-skills/WordScramble/WordScramble/ContentView.swift
@@ -9,23 +9,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    /*
+     
     let people = ["Finn", "Leia", "Luke", "Rey"]
     
     var body: some View {
-        
-        /*
+
          - another way to create a list
          
          List(0..<5) {
              Text("Dynamic row \($0)")
          }
          */
-        List {
+        /*
+                List {
             ForEach(people, id: \.self) {
                 Text($0)
             }
         }
         .listStyle(GroupedListStyle())
+ 
+        
+        
+        
+    }
+    */
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField(
+                    "Enter your word",
+                    text: $newWord,
+                    onCommit: addNewWord
+                )
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // extra validation to come
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
