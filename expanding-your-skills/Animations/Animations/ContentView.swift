4721163a44b2cb5b48c135720bf8286e530fa9f0
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-27 22:16:16 -0300

    update: day 32 content - Customizing animations in SwiftUI

--- expanding-your-skills/Animations/Animations/ContentView.swift
+++ expanding-your-skills/Animations/Animations/ContentView.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Me") {
+            // self.animationAmount += 1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
     }
 }
 
