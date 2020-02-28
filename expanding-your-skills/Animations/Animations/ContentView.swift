dcf372b7275be0b4b35790964a2a49bb5bcc4b28
Nicolau Atala <nicolau.atala@hotmail.com>
2020-02-27 22:04:16 -0300

    feat: add day 32 content - Animations

---
+++ expanding-your-skills/Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Nicolau Atala on 27/02/20.
+//  Copyright © 2020 Nicolau Atala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
