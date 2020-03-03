89eed3f41964f4498a89adc2b7903958b12d3378
Nicolau Atala <nicolau.atala@hotmail.com>
2020-03-02 22:29:50 -0300

    update: day 32 content - Animating bindings

---
+++ expanding-your-skills/Animations/Animations/AnimatingBindings.swift
@@ -0,0 +1,41 @@
+//
+//  AnimatingBindings.swift
+//  Animations
+//
+//  Created by Nicolau Atala on 02/03/20.
+//  Copyright © 2020 Nicolau Atala. All rights reserved.
+//
+
+import SwiftUI
+
+struct AnimatingBindings: View {
+    @State private var animationAmount: CGFloat = 1
+
+    var body: some View {
+        print(animationAmount)
+
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+struct AnimatingBindings_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatingBindings()
+    }
+}
